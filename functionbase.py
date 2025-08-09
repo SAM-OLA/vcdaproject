67a7a2d2ab2f6d1cb04696a143e0f727c5edd958
sam-ola <dapoolaoye@gmail.com>
2025-08-09 17:11:40 +0100

    password textbox updated

--- functionbase.py
+++ functionbase.py
@@ -6,7 +6,7 @@ def connection():
     connn = psycopg2.connect(database="victoryestate", 
                         user="postgres", 
                         password="postgres", 
-                        host="16.171.31.3", port="5432")  #127.0.0.1   16.171.31.3
+                        host="13.62.19.232", port="5432")  #127.0.0.1   16.171.31.3
     return connn
 
 def connectionlite():
