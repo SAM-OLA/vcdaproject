c460100315c54a77eb630f06a2e912ec6c29fba5
sam-ola <dapoolaoye@gmail.com>
2026-06-18 16:28:57 +0100

    GCP connect updated

--- functionbase.py
+++ functionbase.py
@@ -8,7 +8,7 @@ def connection():
             connn = psycopg2.connect(database="victoryestate", 
                         user="postgres", 
                         password="Sqlserver!2", #  postgres Sqlserver!2
-                        host="project-4c43f2db-b2dc-47f4-a89:us-central1:victorydb", port="5432")  #   10.0.2.41
+                        host="/cloudsql/project-4c43f2db-b2dc-47f4-a89:us-central1:victorydb", port="5432")  #   10.0.2.41
             return connn
         except Exception as ee:
             print(f"An error occurred: {str(ee)}")
